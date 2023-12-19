22c1f560c57c79afd6514f65754a566e3d571511
thamyekh <yek3y.ht@gmail.com>
2023-12-19 13:17:06 +1300

    minor fixes

--- optional_installs/install_osep.sh
+++ optional_installs/install_osep.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sudo apt install apt-transport-https gnupg ca-certificates
+sudo DEBIAN_FRONTEND=noninteractive apt -yq install apt-transport-https gnupg ca-certificates
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
 echo "deb https://download.mono-project.com/repo/debian stable-buster main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
 sudo apt update
